3300a9e1dc90335ae24cd022de3ae585fe2265f3
Rafael Fleury <103067158+RafaelFleury@users.noreply.github.com>
2024-05-21 17:32:53 -0300

    Add initial files for Telegram web app

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 window.Telegram.WebApp.ready();
 
+const DemoApp = {
+    initData      : Telegram.WebApp.initData || '',
+    initDataUnsafe: Telegram.WebApp.initDataUnsafe || {},
+    MainButton    : Telegram.WebApp.MainButton,
+
+
+    init(options) {
+        document.body.style.visibility = '';
+        Telegram.WebApp.ready();
+        Telegram.WebApp.MainButton.setParams({
+            text      : 'CLOSE WEBVIEW',
+            is_visible: true
+        }).onClick(DemoApp.close);
+    },
+
+    checkInitData() {
+        const webViewStatus = document.querySelector('#webview_data_status');
+        if (DemoApp.initDataUnsafe.query_id &&
+            DemoApp.initData &&
+            webViewStatus.classList.contains('status_need')
+        ) {
+            webViewStatus.classList.remove('status_need');
+            DemoApp.apiRequest('checkInitData', {}, function (result) {
+                if (result.ok) {
+                    webViewStatus.textContent = 'Hash is correct (async)';
+                    webViewStatus.className   = 'ok';
+                } else {
+                    webViewStatus.textContent = result.error + ' (async)';
+                    webViewStatus.className   = 'err';
+                }
+            });
+        }
+    },
+};
+
+DemoApp.checkInitData();
+DemoApp.init();
+
+
 document.getElementById('userForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -15,3 +54,4 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
 
     Telegram.WebApp.sendData(JSON.stringify(data)); // Envia os dados para o bot do Telegram
 });
+
